5b816aa0325cdd6a25b7575525bdd0dd1acdcf49
Jürgen Heiling <juergen.heiling@gmail.com>
2018-02-22 21:47:27 +0100

    added ECSCapsuleColliders

---
+++ ECS/Unity/Components/ECSCapsuleColliders.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+
+
+namespace ECS
+{
+    [Serializable]
+    public class CapsuleCollidersComponent : IComponent, ICloneable
+    {
+        public CapsuleCollider[] Colliders;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+
+
+    [AddComponentMenu("ECS/Components/CapsuleColliders")]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [DisallowMultipleComponent]
+    public class ECSCapsuleColliders : ComponentWrapper<CapsuleCollidersComponent>
+    {
+        void Awake()
+        {
+            TypedComponent.Colliders = GetComponents<CapsuleCollider>();
+        }
+    }
+}
